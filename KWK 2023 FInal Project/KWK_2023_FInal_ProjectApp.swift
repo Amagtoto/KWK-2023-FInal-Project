cd3b6033ff72bdfe0f53b234a9975da5c0db5316
amagtoto <anmagtoto@gmail.com>
2023-06-26 11:10:29 -0700

    Initial Commit

---
+++ KWK 2023 FInal Project/KWK_2023_FInal_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_2023_FInal_ProjectApp.swift
+//  KWK 2023 FInal Project
+//
+//  Created by Angela Magtoto on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_2023_FInal_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
